e2274e1a91d59e1612d34d7f2846d67fd0765afc
rcharmeyer <rcharmeyer@hotmail.com>
2024-06-26 08:26:39 -0700

    Add some types

--- packages/recon/src/types.ts
+++ packages/recon/src/types.ts
@@ -1,11 +1,21 @@
 import { Func } from "@reconjs/utils"
 
-export type ReconEffect = {}
-
-export type Proc <T = any, A extends any[] = any[]> = (...args: A) => Generator <any, T, any>
-export type Prac <T = any, A extends any[] = any[]> = (...args: A) => AsyncGenerator <any, T, any>
+export type Proc <T = any, A extends any[] = any[]> = (...args: A) => Generator <any, T>
+export type Prac <T = any, A extends any[] = any[]> = (...args: A) => AsyncGenerator <any, T>
 
 export type ProcReturns <P extends Proc> = P extends Proc <infer T> ? T : never
 export type PracReturns <P extends Prac> = P extends Prac <infer T> ? T : never
 
-export type Recon <T = any> = T extends Func ? T : Generator <ReconEffect, T, any>
+export abstract class ReconEffect {}
+
+export abstract class ReconResolver {
+  proc!: Proc
+  params!: any[]
+  current!: any
+}
+
+export abstract class ReconScope {
+  hoist!: Proc <ReconResolver>
+}
+
+export type Recon <T = any> = T extends Func ? T : Generator <ReconEffect, T>
