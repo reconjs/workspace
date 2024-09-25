0576ff46cbf93a7968308bd0eaba01dff04d4f23
Reed Harmeyer <rcharmeyer@hotmail.com>
2024-09-25 19:51:13 +0000

    Add vite plugin

--- examples/hello-world/vite/vite.config.ts
+++ examples/hello-world/vite/vite.config.ts
@@ -1,9 +1,22 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
+import { defineConfig, Plugin } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const noRefresh: Plugin = {
+  name: 'vite:react-no-refresh',
+  config (config) {
+    return {
+      ...config,
+      server: {
+        ...config.server,
+        hmr: false,
+      }
+    }
+  },
+}
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [noRefresh, react(), noRefresh],
   test: {
     globals: true,
     environment: 'jsdom',
